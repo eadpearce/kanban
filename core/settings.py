ecce0d32cf3193790f130912dfd6ebff4b492dae
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2026-01-09 16:33:27 +0000

    Add govuk frontend

--- core/settings.py
+++ core/settings.py
@@ -117,3 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "build" / "static"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "core" / "static",
+]
